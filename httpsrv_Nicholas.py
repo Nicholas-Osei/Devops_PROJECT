0259658470447f65371f403b45e6984310a0adcd
kiddybrain <niconaldodonjunior@yahoo.com>
2020-05-28 23:41:02 +0200

    Update httpsrv_Nicholas.py

--- httpsrv_Nicholas.py
+++ httpsrv_Nicholas.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from flask import Flask,url_for,render_template
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
 
-ledPins = [5,6,16,17,21,22,23,24,26]
+#ledPins = [5,6,16,17,21,22,23,24,26]
 
-for index in ledPins:
- 	GPIO.setup(index, GPIO.OUT)
- 	GPIO.output(index, GPIO.LOW)
+#for index in ledPins:
+ #	GPIO.setup(index, GPIO.OUT)
+ #	GPIO.output(index, GPIO.LOW)
 
 @app.route("/") 
 def home(name=None):
@@ -21,22 +21,22 @@ def home(name=None):
 def ruben(name=None):
 	return render_template('rubens.html')
 
-@app.route("/Leds/<int:number>") 
-def leds(number):
-	for index in ledPins:
-		if index == number:
-			continue
-		GPIO.output(index, GPIO.LOW)
-	if GPIO.input(ledPins[number])==GPIO.HIGH:
-		GPIO.output(ledPins[number],GPIO.LOW)
-	else:
-		GPIO.output(ledPins[number],GPIO.HIGH)
-	if number < 3 :
-		return render_template('rubens.html')
-	elif number < 6 :
-		return render_template('van-hogh.html')
-	else:
-		return render_template('rembrandt.html')
+#@app.route("/Leds/<int:number>") 
+#def leds(number):
+#	for index in ledPins:
+#		if index == number:
+#			continue
+#		GPIO.output(index, GPIO.LOW)
+#	if GPIO.input(ledPins[number])==GPIO.HIGH:
+#		GPIO.output(ledPins[number],GPIO.LOW)
+#	else:
+#		GPIO.output(ledPins[number],GPIO.HIGH)
+#	if number < 3 :
+#		return render_template('rubens.html')
+#	elif number < 6 :
+#		return render_template('van-hogh.html')
+#	else:
+#		return render_template('rembrandt.html')
 
 @app.route("/van-hogh")
 def vanhogh(name=None):
