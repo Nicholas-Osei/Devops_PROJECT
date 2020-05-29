97a9927dadbc56dc71caadda84d26dddda253f05
kiddybrain <niconaldodonjunior@yahoo.com>
2020-05-29 05:25:58 +0200

    Update httpsrv_Nicholas.py

--- httpsrv_Nicholas.py
+++ httpsrv_Nicholas.py
@@ -19,7 +19,7 @@
 
 #import RPi.GPIO as GPIO
 from flask import Flask,url_for,render_template
-app = Flask(__name__,template_folder='template')
+app = Flask(__name__,template_folder='templates')
 
 #GPIO.setmode(GPIO.BCM)
 #GPIO.setwarnings(False)
